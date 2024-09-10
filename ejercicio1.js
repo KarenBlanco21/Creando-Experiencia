1464688dafb4e64ecae73d9f6728ff98fda218ba
Ruth201-US <lorena5peredo7@gmail.com>
2024-09-11 08:29:05 +1200

    arregle lo de las imagenes

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+const respuestacorrect = "falso";
+let respuesta = prompt("ingrese la respuesta si es verdadero o falso");
+
+if (respuestacorrect == respuesta){
+    alert("la respusta es corecta")
+}
+else { respuesta = "verdadero"
+    alert("respuesta incorrecta")
+}
